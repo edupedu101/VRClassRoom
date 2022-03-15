619a4ec199de528d5494f9d54914fe1476f760d4
Carlos Muñoz Rosendo <carmuny54@gmail.com>
2022-03-15 23:04:20 +0100

    monka

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
  
+django_heroku.settings(locals())
