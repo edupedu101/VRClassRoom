f13eb0fa195080790353c207eb3865777f3fd61b
Martí <martillorachnaharro@gmail.com>
2022-03-20 19:14:32 +0100

    Estilo login con tailwind cdd

--- lms/vroom/urls.py
+++ lms/vroom/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
@@ -9,8 +8,9 @@ urlpatterns = [
 
 #autentificacion
 from django.contrib.auth.views import LoginView,LogoutView
+from .forms import CustomLoginForm
 
 urlpatterns += [
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(authentication_form=CustomLoginForm), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
