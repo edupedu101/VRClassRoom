22f4bec8e416fd4a57e22b9ea8a920288f252f7e
Martí <martillorachnaharro@gmail.com>
2022-03-20 09:45:41 +0100

    Login en el frontend con el username

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'vroom.Usuario'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login'
+
 
 
 MIDDLEWARE = [
--- lms/vroom/urls.py
+++ lms/vroom/urls.py
@@ -5,3 +5,15 @@ from . import views
 urlpatterns = [
     path('', views.entrega, name='entrega'),
 ]
+
+
+
+
+
+#autentificacion
+from django.contrib.auth.views import LoginView,LogoutView
+
+urlpatterns += [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
+]
