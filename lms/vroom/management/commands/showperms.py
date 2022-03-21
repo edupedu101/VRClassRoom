f917d4e10b8e604692c2eb8c60e0c6308316ecfe
edupedu101 <edupedu101.eduardvd2001@hotmail.com>
2022-03-21 14:45:25 +0000

    .

--- lms/vroom/management/commands/showperms.py
+++
@@ -1,8 +0,0 @@
-from django.core.management import BaseCommand
-from django.contrib.auth.models import User, Group , Permission
-import logging
-
-class Command(BaseCommand):
-    def handle(self, *args, **option):
-        perms = Permission.objects.all()
-        logging.warning(format(perms))
