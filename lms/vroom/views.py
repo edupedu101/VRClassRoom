f4d2f3d3f33b6da00f413f727eeb90136698f384
Carlos Muñoz Rosendo <carmuny54@gmail.com>
2022-03-21 16:57:52 +0100

    basic css

--- lms/vroom/views.py
+++ lms/vroom/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -6,9 +7,8 @@ from .models import Entrega
 from .models import *
 
 
+def index(request):
+    return render(request, 'vroom/index.html')
+
 def entrega(request):
     return render(request, 'vroom/entrega.html')
-
-def index(request):
-
-    return render(request, 'vroom/index.html')
