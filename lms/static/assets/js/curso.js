e21ee473320137ba077aa4bdffa0bb1fa9ea5386
Martí <martillorachnaharro@gmail.com>
2022-03-27 15:54:44 +0200

    ejercicio vista alumno + mejora curso

--- lms/static/assets/js/curso.js
+++ lms/static/assets/js/curso.js
@@ -1,23 +1,23 @@
-$( () => {
-    $(".contenido").mouseenter( (e) => {
-        if ($(e.target).attr("class") !== 'contenido') return;
-        let icono = $(e.target).find("img");
-        let src = icono.attr('src');
-        src = src.split(".");
-        src.pop();
-        src = src.join() + '.gif';
-        icono.attr('src', src);
-    });
-    $(".contenido").mouseleave( (e) => {
-        if ($(e.target).attr("class") !== 'contenido') return;
-        let icono = $(e.target).find("img");
-        let src = icono.attr('src')
-        src = src.split(".");
-        src.pop();
-        src = src.join() + '.png';
-        icono.attr('src', src);
-    });
-});
+// $( () => {
+//     $(".contenido").mouseenter( (e) => {
+//         if ($(e.target).attr("class") !== 'contenido') return;
+//         let icono = $(e.target).find("img");
+//         let src = icono.attr('src');
+//         src = src.split(".");
+//         src.pop();
+//         src = src.join() + '.gif';
+//         icono.attr('src', src);
+//     });
+//     $(".contenido").mouseleave( (e) => {
+//         if ($(e.target).attr("class") !== 'contenido') return;
+//         let icono = $(e.target).find("img");
+//         let src = icono.attr('src')
+//         src = src.split(".");
+//         src.pop();
+//         src = src.join() + '.png';
+//         icono.attr('src', src);
+//     });
+// });
 
 function goToEjercicio(id_ejercicio){
     console.log(`#ejercicio-${id_ejercicio}`)
